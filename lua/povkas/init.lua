dd2e67d109ad5973a0524cd3f05bff7deb358b54
Povilas Krasavinas <p.krasavinas@gmail.com>
2023-04-14 21:06:53 +0300

    init

---
+++ lua/povkas/init.lua
@@ -0,0 +1,3 @@
+
+require("povkas.set")
+require("povkas.remap")
---
+++ lua/povkas/remap.lua
@@ -0,0 +1,12 @@
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", {})
+vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", {})
+vim.api.nvim_set_keymap("n", "<C-f>", "<C-f>zz", {})
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
