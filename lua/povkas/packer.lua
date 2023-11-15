083d463d72512b0b48e9ab0c44fc0362a7a58241
Povilas Krasavinas <p.krasavinas@gmail.com>
2023-11-15 21:40:01 +0200

    cleanup

--- lua/povkas/packer.lua
+++ lua/povkas/packer.lua
@@ -5,42 +5,38 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use('wbthomason/packer.nvim')
 
-  use {
+  use({
     'nvim-telescope/telescope.nvim', tag = '0.1.2',
-  -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
-  }
+  })
 
-  use 'ThePrimeagen/vim-be-good'
+  use('ThePrimeagen/vim-be-good')
 
   use('MunifTanjim/prettier.nvim')
 
   use('sbdchd/neoformat')
+
   use('folke/tokyonight.nvim')
 
-  use {
+  use({
       'nvim-lualine/lualine.nvim',
       requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-
-  vim.cmd[[colorscheme tokyonight-night]]
+  })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('theprimeagen/harpoon')
 
+  use('theprimeagen/harpoon')
 
   use('tpope/vim-fugitive')
 
-  use {
+  use({
       'VonHeikemen/lsp-zero.nvim',
       branch = 'v3.x',
       requires = {
-          --- Uncomment these if you want to manage LSP servers from neovim
           {'williamboman/mason.nvim'},
           {'williamboman/mason-lspconfig.nvim'},
-
           -- LSP Support
           {'neovim/nvim-lspconfig'},
           -- Autocompletion
@@ -48,6 +44,6 @@ return require('packer').startup(function(use)
           {'hrsh7th/cmp-nvim-lsp'},
           {'L3MON4D3/LuaSnip'},
       }
-}
+})
 end)
 
